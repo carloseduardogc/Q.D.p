5826e98354c750ce94dfad3d63b5a63d5e1e3b34
Carlos eduardo Gonçalves canuto <carloseduardo17102018@gmail.com>
2023-11-07 07:41:43 -0300

    create python file

---
+++ main.py
@@ -0,0 +1,5 @@
+nota1 = float(input('qual sua nota 1:'))
+nota2 = float(input('qual sua nota 2:'))
+nota3 = float(input('qual sua nota 3:'))
+nota4 = float(input('qual sua nota 4:'))
+print("sua média é",(nota1+nota2+nota3+nota4)/4)
